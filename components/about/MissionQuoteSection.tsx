6de08750fb5490cd2673e9e0aeee62e31eeb81db
vihanga <vihanganethusara00@gmail.com>
2025-10-29 00:49:38 +0530

    feat: mission

---
+++ components/about/MissionQuoteSection.tsx
@@ -0,0 +1,11 @@
+export default function MissionQuoteSection() {
+  return (
+    <section className="flex items-center justify-center">
+      <div className="py-16 px-4 bg-[#55C514] max-w-7xl text-center rounded-[10px] w-full flex flex-col items-center justify-center">
+           <blockquote className="text-xl md:text-2xl font-semibold max-w-[793px] italic leading-relaxed">
+            “Our mission is to craft unforgettable brand stories that resonate, engage, and convert.”
+          </blockquote>
+      </div>
+    </section>
+  );
+}
