9a7cff20690f300348a8112e89137625a27068eb
vihanga <vihanganethusara00@gmail.com>
2025-10-28 15:42:37 +0530

    feat: adding why partner with us to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import WhyPartner from "@/components/WhyPartner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+        <WhyPartner />
+      </main>
     </>
   );
 }
