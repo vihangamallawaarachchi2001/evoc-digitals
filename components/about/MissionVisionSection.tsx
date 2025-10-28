0b4d3b4f74b55eee47d95f115e38a3b443711ea6
vihanga <vihanganethusara00@gmail.com>
2025-10-29 00:50:08 +0530

    feat: mission vission

--- components/about/MissionVisionSection.tsx
+++ components/about/MissionVisionSection.tsx
@@ -8,44 +8,44 @@ export default function MissionVisionSection() {
     <section className="py-16 px-4 ">
       <div className="w-full mx-auto max-w-7xl">
         <div className="grid md:grid-cols-2 gap-8">
-          <div className="bg-white text-black p-6 rounded-xl shadow-md">
+          <div className=" p-6 rounded-xl shadow-md">
             <div className="flex items-center mb-4">
-              <ChartLine className="text-green-500 mr-2 text-2xl" />
-              <h3 className="text-2xl font-bold text-green-500">Our Mission</h3>
+              <ChartLine className="text-[#55C514] mr-2 text-2xl" />
+              <h3 className="text-2xl text-[#55C514] font-bold ">Our Mission</h3>
             </div>
             <p className="mb-4">
               To empower brands with creative, data-driven advertising solutions that captivate audiences, foster growth, and deliver measurable results. We are dedicated to pushing the boundaries of digital marketing through innovation and strategic excellence.
             </p>
             <div className="flex space-x-2 mt-4">
-              <div className="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
-                <ChartLine className="text-green-500" />
+              <div className="w-12 h-12 bg-[#55C514]  rounded-full flex items-center justify-center">
+                <ChartLine className="" />
               </div>
-              <div className="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
-                <Eye className="text-green-500" />
+              <div className="w-12 h-12 bg-[#55C514]  rounded-full flex items-center justify-center">
+                <Eye className="" />
               </div>
-              <div className="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
-                <Globe className="text-green-500" />
+              <div className="w-12 h-12 bg-[#55C514] rounded-full flex items-center justify-center">
+                <Globe className="" />
               </div>
             </div>
           </div>
 
-          <div className="bg-white text-black p-6 rounded-xl shadow-md">
+          <div className=" p-6 rounded-xl shadow-md">
             <div className="flex items-center mb-4">
-              <Globe className="text-green-500 mr-2 text-2xl" />
-              <h3 className="text-2xl font-bold text-green-500">Our Vision</h3>
+              <Globe className="text-[#55C514] mr-2 text-2xl" />
+              <h3 className="text-2xl text-[#55C514] font-bold ">Our Vision</h3>
             </div>
             <p className="mb-4">
               To be the leading digital advertising catalyst, recognized for transforming brands into market leaders. We aspire to create a future where every brand story is not just told, but experienced, leaving a lasting impact on a global scale.
             </p>
             <div className="flex space-x-2 mt-4">
-              <div className="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
-                <Globe className="text-green-500" />
+              <div className="w-12 h-12 bg-[#55C514] rounded-full flex items-center justify-center">
+                <Globe className="" />
               </div>
-              <div className="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
-                <ChartLine className="text-green-500" />
+              <div className="w-12 h-12 bg-[#55C514] rounded-full flex items-center justify-center">
+                <ChartLine className="" />
               </div>
-              <div className="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
-                <Eye className="text-green-500" />
+              <div className="w-12 h-12 bg-[#55C514] rounded-full flex items-center justify-center">
+                <Eye className="" />
               </div>
             </div>
           </div>
